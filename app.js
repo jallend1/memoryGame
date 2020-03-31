2a6c54b749091326757bd6862bf1540bb16f89ac
jallend1 <jallend1@gmail.com>
2020-03-30 17:45:50 -0700

    Nearly from-the-ground rebuild now with limited functionalityi

--- app.js
+++ app.js
@@ -1,63 +1,46 @@
 //  Cards from http://acbl.mybigcommerce.com/52-playing-cards/
 
 const gameArea = document.querySelector('#gamearea');
-const scoreBoard = document.querySelector('#score');
-let displayOrder;
 let cardColor = 'green';
 const cardBack = `images/card${cardColor}.png`;
-const cards = [];                                                           // Array of all the available cards
 const revealedCards = [];                                                    // Array of the _revealed_ cards
-let score = 0;
+const cardNames = [];                                                         // Array of all the available cards
+const scoreBoard = document.querySelector('#score');
 
-function checkMatch(){
-    let cardOne = revealedCards[0];
-    let cardTwo = revealedCards[1];
-    let oneLocation = cards.indexOf(cardOne);
-    let twoLocation = cards.indexOf(cardTwo);
-    if(cardOne === cardTwo){
-        score++;
-        scoreBoard.innerText = score;
-        displayOrder[oneLocation].classList.add('success');
-        displayOrder[twoLocation].classList.add('success');
-    }else{
-        setTimeout(() => {
-            displayOrder[oneLocation].setAttribute('src', cardBack);
-            displayOrder[twoLocation].setAttribute('src', cardBack);
-            console.log(displayOrder[oneLocation]);
-            console.log(displayOrder[twoLocation]);
-        }, 500);
-    }
-    revealedCards.length = 0;
-}
 
-function createCardsInArray(){                                              // Puts duplicates of every card into the array
+function generateCards() {                                              // Puts duplicates of every card into an array and randomizes order
     for(let i = 1; i < 14; i++){
-        cards.push(`${i}C`, `${i}C`);
-        cards.push(`${i}H`, `${i}H`);        
+        cardNames.push(`${i}C`, `${i}C`);
+        cardNames.push(`${i}H`, `${i}H`);        
     }
-    cards.sort(() => 0.5 - Math.random());                                  // Puts the cards in a random order
+    cardNames.sort(() => 0.5 - Math.random());
 }
 
-function populateCards(){                                                   // Populates the back of the cards on screen
-    cards.forEach(card => {
+function populateCards(){                                                   // Converts card names into card elements
+    cardNames.forEach((card, index) => {
         const newCard = document.createElement('img');
         newCard.setAttribute('src', cardBack);                              // Sets image to back of the card
-        newCard.setAttribute('data-card', card);
+        newCard.setAttribute('data-card', card);                            // Sets the data attribute of each element to its face value
+        newCard.setAttribute('data-index', index);
         newCard.classList.add('card');                                      // Applies styling to the new card
         gameArea.appendChild(newCard);                                      // Adds card to div
         newCard.addEventListener('click', e => revealCard(e));
     });
-    return document.querySelectorAll('.card');
 }
 
 function revealCard(e){                                                     // Flips the card over
     let selectedCard = e.target.dataset.card;
-    e.target.setAttribute('src', `images/${selectedCard}.png`);
+    e.target.setAttribute('src', `images/${selectedCard}.png`);             // Shows card image associated with data-card
     revealedCards.push(selectedCard);
     if (revealedCards.length === 2){
         checkMatch();
+        revealedCards.length = 0;
     }    
 }
 
-createCardsInArray()
-displayOrder = populateCards();
+function checkMatch(){
+    console.log(cardNames.item(index));
+}
+
+generateCards();
+populateCards();
